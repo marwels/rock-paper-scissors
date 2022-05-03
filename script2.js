0af00edfbed8eff44a6c401b64861f96d9e96be7
Marta Welfe-Starzak <mwelfe@gmail.com>
2022-05-03 11:41:52 +0200

    add function computerPlay()
    randomly returns either 1, 2 or 3

---
+++ script2.js
@@ -0,0 +1,14 @@
+function computerPlay() {
+    return Math.floor(Math.random() * (3 - 1 + 1) + 1);
+    // randomly returns either 1, 2 or 3
+    //1 rock
+    //2 paper
+    //3 scissors
+}
+
+//function randomIntFromInterval(min, max) { // min and max included 
+//    return Math.floor(Math.random() * (max - min + 1) + min)
+//}
+//const rndInt = randomIntFromInterval(1, 6)
+//console.log(rndInt)
+
