b8378c92602266f1cc31ee2c995c1785fc75cdc1
Marta Welfe-Starzak <mwelfe@gmail.com>
2022-05-03 12:02:26 +0200

    finnish function playRound
    add condition to compare playerSelection and computerSelection

--- script2.js
+++ script2.js
@@ -26,5 +26,23 @@ function playRound(playerSelection, computerSelection) {
         console.log("Sorry, you've made a spelling mistake. Try again!");
     }
 
+
+    if (playerSelection === computerSelection) {
+        return "It's a tie!";
+    } else if (playerSelection === 1 && computerSelection === 2) {
+        return "You Lose! Paper beats Rock";
+    } else if (playerSelection === 1 && computerSelection === 3) {
+        return "You Win! Rock beats Scissors";
+    } else if (playerSelection === 2 && computerSelection === 1) {
+        return "You Win! Paper beats Rock";
+    } else if (playerSelection === 2 && computerSelection === 3) {
+        return "You Lose! Scissors beats Paper";
+    } else if (playerSelection === 3 && computerSelection === 1) {
+        return "You Lose! Rock beats Scissors";
+    } else if (playerSelection === 3 && computerSelection === 2) {
+        return "You Win! Scissors beats Paper";
+    } else { return "You have to choose 'Rock', 'Paper' or 'Scissors'" }
 }
+
+
 
