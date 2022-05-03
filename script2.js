cded0869b18a96248d78abc71a89f14eba86f688
Marta Welfe-Starzak <mwelfe@gmail.com>
2022-05-03 11:53:32 +0200

    add var computerSelection and start function playRound
    playerSelection is brought toUpperCase to make it case insensitive
    tahn it's changed into numbers to avoid spelling mistakes

--- script2.js
+++ script2.js
@@ -5,10 +5,26 @@ function computerPlay() {
     //2 paper
     //3 scissors
 }
+let computerSelection = computerPlay();
 
 //function randomIntFromInterval(min, max) { // min and max included 
 //    return Math.floor(Math.random() * (max - min + 1) + min)
 //}
 //const rndInt = randomIntFromInterval(1, 6)
 //console.log(rndInt)
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toUpperCase();
+
+    if (playerSelection === "ROCK") {
+        playerSelection = 1;
+    } else if (playerSelection === "PAPER") {
+        playerSelection = 2;
+    } else if (playerSelection === "SCISSORS") {
+        playerSelection = 3;
+    } else {
+        console.log("Sorry, you've made a spelling mistake. Try again!");
+    }
+
+}
 
